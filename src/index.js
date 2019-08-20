dcdedef6dde6db62b759863f158b2b168165d997
jose restrepo <arturo.restrepo@gmail.com>
2019-08-20 14:40:12 -0500

    initial commit, app enabled

--- src/index.js
+++ src/index.js
@@ -1,8 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-
 import './index.css';
+import App from './App';
+import * as serviceWorker from './serviceWorker';
 
-import Dashboard from './dashboard/Dashboard';
+ReactDOM.render(<App />, document.getElementById('root'));
 
-ReactDOM.render(<Dashboard />, document.getElementById('root'));
+serviceWorker.unregister();
