2de8c03e6a2c3b54d64696c64557439ca3e1c115
jose restrepo <arturo.restrepo@gmail.com>
2019-08-20 15:23:16 -0500

    refactored MVP completed

--- src/dashboard/Dashboard.spec.js
+++ src/dashboard/Dashboard.spec.js
@@ -1,1 +1,65 @@
 // Test away
+import React from 'react';
+import { render, fireEvent } from '@testing-library/react';
+
+import Dashboard from './Dashboard';
+
+describe('Dashboard /> functionality after closed', () => {
+
+    it('registers as closed, lock gate is not disabled', () => {
+  
+      const { queryByText, container } = render(<Dashboard />);
+  
+      const unlockedIndicator = queryByText(/unlocked/i);
+      expect(unlockedIndicator.classList.contains('green-led')).toBeTruthy();
+  
+      const openIndicator = queryByText(/open/i);
+      expect(openIndicator.classList.contains('green-led')).toBeTruthy();
+  
+      expect(container.querySelector('led red-led')).toBeFalsy();
+      expect(container.querySelectorAll('led green-led')).toBeTruthy();
+  
+      expect(queryByText(/open gate/i)).toBeFalsy();
+      expect(queryByText(/close gate/i)).toBeTruthy();
+      expect(queryByText(/closed/i)).toBeFalsy();
+      expect(queryByText(/lock gate/i).hasAttribute('disabled')).toBeTruthy();
+      expect(queryByText(/open/i)).toBeTruthy();
+  
+      fireEvent.click(queryByText(/close gate/i));
+  
+      const closedIndicator = queryByText(/closed/i)
+      expect(closedIndicator.classList.contains('red-led')).toBeTruthy();
+  
+      expect(unlockedIndicator.classList.contains('green-led')).toBeTruthy();
+  
+      expect(queryByText(/open gate/i)).toBeTruthy();
+      expect(queryByText(/close gate/i)).toBeFalsy();
+      expect(queryByText(/closed/i)).toBeTruthy();
+      expect(queryByText(/open`[ gate]/i)).toBeFalsy();
+  
+    })
+  })
+  
+  describe('Dashboard /> functionality after locked', () => {
+  it('cannot be opened or closed after locked', () => {
+    const { queryByText } = render(<Dashboard />);
+
+    expect(queryByText(/unlock gate/i)).toBeFalsy();
+    expect(queryByText(/[^un]locked/i)).toBeFalsy();
+    expect(queryByText(/lock gate/i).hasAttribute('disabled')).toBeTruthy();
+
+    fireEvent.click(queryByText(/close gate/i));
+    fireEvent.click(queryByText(/lock gate/i));
+
+    const closedIndicator = queryByText(/closed/i)
+    expect(closedIndicator.classList.contains('red-led')).toBeTruthy();
+
+    const lockedIndicator = queryByText(/locked/i)
+    expect(lockedIndicator.classList.contains('red-led')).toBeTruthy();
+
+    expect(queryByText(/unlock gate/i)).toBeTruthy();
+    expect(queryByText(/locked/i)).toBeTruthy();
+    expect(queryByText(/open gate/i).hasAttribute('disabled')).toBeTruthy();
+    expect(queryByText(/open gate/i).hasAttribute('disabled')).toBeTruthy();
+  });
+});
--- src/index.js
+++ src/index.js
@@ -1,9 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+
 import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+import Dashboard from './dashboard/Dashboard';
 
-serviceWorker.unregister();
+ReactDOM.render(<Dashboard />, document.getElementById('root'));
